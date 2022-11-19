de539eb7a8c825cb95510871d5d2f6e55f84c2fd
kimhsn <hakimhassaine@univ-rouen.fr>
2022-11-19 11:16:39 +0100

    fixed bugs

--- AppDotNet/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
+++ AppDotNet/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using AppDotNet.Areas.Identity.Data;
+using AppDotNet.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
